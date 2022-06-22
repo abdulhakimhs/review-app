446c9fbef6ac80def15810d413a2755e27dbb706
abdulhakimhs <abdul.hakim.pekl@gmail.com>
2022-06-22 07:13:40 +0700

    Layout confirm password

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {BsFillSunFill} from 'react-icons/bs'
+import { BsFillSunFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 import Container from '../Container'
 
 export default function Navbar() {
@@ -7,18 +8,22 @@ export default function Navbar() {
     <div className="bg-secondary shadow-sm shadow-gray-500">
       <Container className="p-2">
         <div className="flex justify-between items-center">
-          <img src="./logo.png" alt="" className='h-10' />
+          <Link to="/">
+            <img src="./logo.png" alt="" className='h-10' />
+          </Link>
 
           <ul className='flex items-center space-x-4'>
             <li>
               <button className='bg-dark-subtle p-1 rounded'>
-                <BsFillSunFill className='text-secondary' size={24}/>
+                <BsFillSunFill className='text-secondary' size={24} />
               </button>
             </li>
             <li>
               <input type="text" className='border-2 border-dark-subtle p-1 rounded bg-transparent text-xl outline-none focus:border-white transition text-white' placeholder='search...' />
             </li>
-            <li className='text-white font-semibold text-lg'>Login</li>
+            <li className='text-white font-semibold text-lg'>
+              <Link to="auth/signin">Login</Link>
+            </li>
           </ul>
         </div>
       </Container>
--- frontend/src/components/user/auth/ConfirmPassword.jsx
+++ frontend/src/components/user/auth/ConfirmPassword.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import Container from '../../Container'
+import FormInput from '../../form/FormInput'
+import Submit from '../../form/Submit'
+import Title from '../../form/Title'
 
 export default function ConfirmPassword() {
   return (
-    <div>ConfirmPassword</div>
+    <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 w-96 space-y-6">
+          <Title>Enter New Password</Title>
+          <FormInput
+            type="password"
+            name="password"
+            placeholder="********"
+            label="New Password"
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            placeholder="********"
+            label="Confirm Password"
+          />
+          <Submit value="Confirm Password" />
+        </form>
+      </Container>
+    </div>
   )
 }
